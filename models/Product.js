dfc55ea630fe96452ff207738592e0caabad8bc0
Rex Yu <yu19910513@gmail.com>
2021-06-12 10:48:24 -0700

    api

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // References the Category model's id.
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,22 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+      // References the Product model's id.
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+      // References the Tag model's id.
+    }
   },
   {
     sequelize,
