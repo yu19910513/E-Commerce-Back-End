fa6b78d92b414165f88ced51e39ccc613f9529dd
Rex Yu <yu19910513@gmail.com>
2021-06-15 21:34:16 -0700

    final

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {foreignKey: "categorry_id"});
+Product.belongsTo(Category, {foreignKey: "category_id"});
 
 
 // Categories have many Products
